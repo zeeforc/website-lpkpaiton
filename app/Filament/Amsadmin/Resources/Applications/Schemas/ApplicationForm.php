0e0c9b242c95d2d2be45a5de8eb5c1d3ee05f25a
zeeforc <zeeforc2@gmail.com>
2026-09-21 10:59:35 +0700

    fix: correct namespace for Section in ApplicationForm

--- app/Filament/Amsadmin/Resources/Applications/Schemas/ApplicationForm.php
+++ app/Filament/Amsadmin/Resources/Applications/Schemas/ApplicationForm.php
@@ -8,7 +8,7 @@ use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Section;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class ApplicationForm
